801c162d7441acbfe8657b2c9f0a424d082ba0b4
Axel Hernandez <axeelhrz@gmail.com>
2026-06-01 00:01:32 -0300

    fix

--- components/dashboard/DashboardViewer.tsx
+++ components/dashboard/DashboardViewer.tsx
@@ -1999,8 +1999,6 @@ export function DashboardViewer({
                     display: "flex",
                     flexDirection: "column",
                     minHeight: 0,
-                    minWidth: 0,
-                    overflow: useClientTheme ? "hidden" : undefined,
                     position: "relative",
                     zIndex: typeof (widget as Widget).zIndex === "number" ? (widget as Widget).zIndex : 0,
                     ...cellThemeVars,
